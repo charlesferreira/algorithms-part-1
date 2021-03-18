c7c7c900a2e615d3c0af8839ab77b46b2bfda8f8
Charles Ferreira <charles.dev@live.com>
2021-03-18 14:35:18 -0300

    Create a simple class to try out the API

---
+++ 02-UnionFind/src/lecture/ExampleAPI.java
@@ -0,0 +1,22 @@
+package lecture;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class ExampleAPI {
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.find(p) != uf.find(q)) {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
+            }
+        }
+    }
+
+}
